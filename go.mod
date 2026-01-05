ee5a908fc32ba21d4b9ab4c3cc8947ba2448f113
Wojciech Małota-Wójcik <59281144+outofforest@users.noreply.github.com>
2026-01-05 21:49:11 +0100

    Implementation (#2)

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/outofforest/archive
 
 go 1.24.0
+
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/ulikunitz/xz v0.5.15
+)
---
+++ helpers.go
@@ -0,0 +1,44 @@
+package archive
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+func ensureDir(dir string) (string, error) {
+	if err := os.MkdirAll(dir, 0o700); err != nil {
+		return "", errors.WithStack(err)
+	}
+	dir, err := filepath.Abs(dir)
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+	dir, err = filepath.EvalSymlinks(dir)
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+
+	return dir, nil
+}
+
+func ensureFileInDir(dir, file string) (string, error) {
+	file = filepath.Join(dir, file)
+	rel, err := filepath.Rel(dir, file)
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+	if strings.HasPrefix(rel, "..") {
+		return "", errors.Errorf("file %s is outside base directory %s", file, dir)
+	}
+	return file, nil
+}
+
+func ensureFileDir(file string) error {
+	if err := os.MkdirAll(filepath.Dir(file), 0o700); err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
